1f7e785456af6a8575d71b16b897f51281236641
zxfonline <zxfonline@sina.com>
2017-04-13 10:15:15 +0800

    format

--- sorter.go
+++ sorter.go
@@ -1,6 +1,7 @@
 // Copyright 2015 someonegg. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 package ulua
 
 import (
